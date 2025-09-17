72d8548909fdb3a4112d400b8695ab081fac6791
DDWVT <dewa.vito2005@gmail.com>
2025-09-17 16:59:24 +0700

    Commit No.5 – add back button & extend list

--- app/src/main/java/com/example/lab_week_03/DetailFragment.kt
+++ app/src/main/java/com/example/lab_week_03/DetailFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import android.widget.Button
 
 class DetailFragment : Fragment() {
 
@@ -21,6 +22,7 @@ class DetailFragment : Fragment() {
 
         val coffeeTitle = view.findViewById<TextView>(R.id.coffee_title)
         val coffeeDesc = view.findViewById<TextView>(R.id.coffee_desc)
+        val backButton = view.findViewById<Button>(R.id.back_button)
 
         val coffeeId = arguments?.getInt(ListFragment.COFFEE_ID) ?: 0
 
@@ -37,6 +39,18 @@ class DetailFragment : Fragment() {
                 coffeeTitle.text = getString(R.string.latte_title)
                 coffeeDesc.text = getString(R.string.latte_desc)
             }
+            R.id.espresso -> {
+                coffeeTitle.text = getString(R.string.espresso_title)
+                coffeeDesc.text = getString(R.string.espresso_desc)
+            }
+            R.id.cappuccino -> {
+                coffeeTitle.text = getString(R.string.cappuccino_title)
+                coffeeDesc.text = getString(R.string.cappuccino_desc)
+            }
+        }
+
+        backButton.setOnClickListener {
+            parentFragmentManager.popBackStack()
         }
     }
 }
